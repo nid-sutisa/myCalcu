42af1265597335bf7e5bd9aaef24876f2b0c0128
sutisa <a_sutisa@snru.ac.th>
2017-06-13 10:27:22 +0700

    MorningBeak13June2017

--- My Calculate/ViewController.swift
+++ My Calculate/ViewController.swift
@@ -9,11 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Implicit
+    var intNumber1 = 0
+    var intNumber2: Int = 0
+    var strNumber1 = ""
+    var strNumber2: String = ""
+    
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var num1TextField: UITextField!
+    
+    @IBOutlet weak var num2TextField: UITextField!
+    
+    @IBAction func answerButtom(_ sender: Any) {
+        strNumber1 = num1TextField.text!
+        strNumber2 = num2TextField.text!
+        
+        //show log
+        print("strNumber1 ==> \(strNumber1)")
+        print("strNumber2 ==> \(strNumber2)")
+        print("Answer ==> \(Int(strNumber1)!+Int(strNumber2)!)")
+        
+        
+        
+    } //answerButtom
+    
+  
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    } //Main method and first work
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,5 +54,5 @@ class ViewController: UIViewController {
     }
 
 
-}
+} //Main class
 
